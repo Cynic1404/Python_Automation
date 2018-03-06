0e1bcd5ac78e33609b4b8767bbb2b760a81e472c
Nekhaenok <nekhaenok@gmail.com>
2018-03-05 22:08:34 -0800

    parameters created

--- test_add_group.py
+++ test_add_group.py
@@ -20,51 +20,61 @@ class test_add_group(unittest.TestCase):
         wd.get("http://localhost/addressbook/")
 
 
-    def login(self, wd):
+    def login(self, wd, username, password):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-
-
-    def logout(self, wd):
-        # logout
-        wd.find_element_by_link_text("Logout").click()
-
-
-    def return_group_page(self, wd):
-        # return to group pgae
-        wd.find_element_by_link_text("group page").click()
-
-
-    def create_group(self, wd):
-        wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("My_first_group")
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("Header")
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("Footer")
-        wd.find_element_by_name("submit").click()
 
 
     def open_groups_page(self, wd):
         wd.find_element_by_link_text("groups").click()
 
 
-    def test_test_add_group(self):
-        success = True
+    def create_group(self, wd, group_name, header, footer):
+        # start to create a group
+        wd.find_element_by_name("new").click()
+        # fill forms
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group_name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(footer)
+        # submit group creation
+        wd.find_element_by_name("submit").click()
+
+
+    def return_group_page(self, wd):
+        wd.find_element_by_link_text("group page").click()
+
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+
+    def test_add_group(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
+        self.login(wd, username="admin", password="secret")
         self.open_groups_page(wd)
-        self.create_group(wd)
+        self.create_group(wd, group_name="My_first_group", header="Header", footer="Footer")
+        self.return_group_page(wd)
+        self.logout(wd)
+
+
+    def test_add_empty_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_groups_page(wd)
+        self.create_group(wd, group_name="", header="", footer="")
         self.return_group_page(wd)
         self.logout(wd)
 
