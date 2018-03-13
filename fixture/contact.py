71f2f665c18c810b8bc4c298a7865163fb15e5d3
Nekhaenok <nekhaenok@gmail.com>
2018-03-12 22:21:08 -0700

    Homework 7 (2-4) done edit contact test case added

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,13 +1,10 @@
-from time import sleep
 class ContactHelper:
-
     def __init__(self, app):
         self.app = app
 
     def open_add_contact_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("add new").click()
-
 
 
     def add(self, contact):
@@ -36,6 +33,31 @@ class ContactHelper:
         # confirm adding contact
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
         self.return_contacts_page()
+
+
+    def edit_first_contact(self, contact):
+        wd = self.app.wd
+        wd.find_element_by_xpath("//table[@id='maintable']/tbody/tr[2]/td[8]/a/img").click()
+        # fill forms
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.first_name)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.last_name)
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(contact.nickname)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact.home_telephone)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.email)
+        wd.find_element_by_name("homepage").click()
+        wd.find_element_by_name("homepage").clear()
+        wd.find_element_by_name("homepage").send_keys(contact.homepage)
+        wd.find_element_by_name("update").click()
+
 
     def delete_first_contact(self):
         wd = self.app.wd
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -2,7 +2,7 @@
 from model.group import Group
 
 
-def test_add_group(app):
+def test_edit_first_group(app):
     app.session.login(username="admin", password="secret")
     app.group.edit_first_group(Group(group_name="Name edited", header="Header edited", footer="Footer edited"))
     app.session.logout()
