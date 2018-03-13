38facadece9ff3fa7df00b42c5cb892e58950a93
Nekhaenok <nekhaenok@gmail.com>
2018-03-12 18:43:58 -0700

    Decomposition

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-from contact import Contact
-from application import Application
 import pytest
 
+from contact import Contact
+from fixture.application import Application
 
 
 @pytest.fixture
