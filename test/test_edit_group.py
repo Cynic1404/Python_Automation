da4631a36df711632130c707c6104983a11f3001
Nekhaenok <nekhaenok@gmail.com>
2018-03-12 21:34:03 -0700

    test_edit_first_group

---
+++ test/test_edit_group.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from model.group import Group
+
+
+def test_add_group(app):
+    app.session.login(username="admin", password="secret")
+    app.group.edit_first_group(Group(group_name="Name edited", header="Header edited", footer="Footer edited"))
+    app.session.logout()
