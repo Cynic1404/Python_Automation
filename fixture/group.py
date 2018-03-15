2f046c472ecb4f2d278f2fe294525e99689f06be
Nekhaenok <nekhaenok@gmail.com>
2018-03-14 23:48:10 -0700

    fill_forms method added

--- fixture/group.py
+++ fixture/group.py
@@ -7,12 +7,8 @@ class GroupHelper:
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
 
-    def create(self, group):
+    def fill_forms(self, group):
         wd = self.app.wd
-        self.open_groups_page()
-        # start to create a group
-        wd.find_element_by_name("new").click()
-        # fill forms
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.group_name)
@@ -22,6 +18,14 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
+
+    def create(self, group):
+        wd = self.app.wd
+        self.open_groups_page()
+        # start to create a group
+        wd.find_element_by_name("new").click()
+        # fill forms
+        self.fill_forms(group)
         # submit group creation
         wd.find_element_by_name("submit").click()
         self.return_groups_page()
@@ -33,15 +37,7 @@ class GroupHelper:
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_name("edit").click()
         # fill forms
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.group_name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.fill_forms(group)
         # submit group creation
         wd.find_element_by_name("update").click()
         self.return_groups_page()
