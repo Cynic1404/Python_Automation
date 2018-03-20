d70f47c4afbf805a4c5354cf154a46662ddb1728
Nekhaenok <nekhaenok@gmail.com>
2018-03-19 19:09:21 -0700

    3-2

--- fixture/group.py
+++ fixture/group.py
@@ -9,17 +9,21 @@ class GroupHelper:
         wd.find_element_by_link_text("groups").click()
 
 
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
+
+
+
+
     def fill_group_form(self, group):
         wd = self.app.wd
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.group_name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.change_field_value("group_name", group.group_name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group_footer", group.footer)
 
 
     def create(self, group):
@@ -34,14 +38,14 @@ class GroupHelper:
         self.return_groups_page()
 
 
-    def modify_first_group(self, group):
+    def modify_first_group(self, new_group_data):
         wd = self.app.wd
         self.open_groups_page()
         # start to edit first group
         self.select_first_group()
         wd.find_element_by_name("edit").click()
         # fill forms
-        self.fill_group_form(group)
+        self.fill_group_form(new_group_data)
         # submit group creation
         wd.find_element_by_name("update").click()
         self.return_groups_page()
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,8 +1,21 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 
-
 def test_edit_first_group(app):
     app.session.login(username="admin", password="secret")
     app.group.modify_first_group(Group(group_name="Edited group", header="Header edited", footer="Footer edited"))
+    app.session.logout()
+
+
+
+def test_modify_group_name(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(group_name="New name"))
+    app.session.logout()
+    
+
+
+def test_modify_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(footer="New header"))
     app.session.logout()
