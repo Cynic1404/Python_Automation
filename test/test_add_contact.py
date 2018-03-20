7d44cd1faebfa4312c2fcc579ce9cbcd23972280
Nekhaenok <nekhaenok@gmail.com>
2018-03-19 19:19:38 -0700

    Login, logout added to fixture

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,12 +3,10 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.add_new_contact(Contact(first_name="My first", last_name="Contact", nickname="Mr.First", home_telephone="+79261111111", email="test@gmail.com", homepage="www.pythontraining.com"))
-    app.session.logout()
+
 
 
 def test_add_empty_contact(app):
-    app.session.login("admin", "secret")
     app.contact.add_new_contact(Contact(first_name="", last_name="", nickname="", home_telephone="", email="", homepage=""))
-    app.session.logout()
+
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,14 +3,12 @@ from model.group import Group
 
 
 def test_add_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(group_name="My_first_group", header="Header", footer="Footer"))
-    app.session.logout()
+
 
 
 def test_add_empty_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(group_name="", header="", footer=""))
-    app.session.logout()
+
 
 
