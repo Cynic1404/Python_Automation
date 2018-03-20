16410218e732bc811e11f3436f92a728aad241cc
Nekhaenok <nekhaenok@gmail.com>
2018-03-19 20:32:33 -0700

    Homework 8 (3-1) done

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -6,7 +6,6 @@ def test_add_group(app):
     app.group.create(Group(group_name="My_first_group", header="Header", footer="Footer"))
 
 
-
 def test_add_empty_group(app):
     app.group.create(Group(group_name="", header="", footer=""))
 
