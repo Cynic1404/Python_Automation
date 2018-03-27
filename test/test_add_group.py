817c58e0e38f97327a2d26a19f8c63dfa5c4cd63
Nekhaenok <nekhaenok@gmail.com>
2018-03-26 20:17:54 -0700

    Проверки добавлены

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,8 +3,13 @@ from model.group import Group
 
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(group_name="My_first_group", header="Header", footer="Footer"))
-
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) +1 == len(new_groups)
 
 def test_add_empty_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(group_name="", header="", footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -5,7 +5,9 @@ def test_edit_first_group(app):
     if app.group.count() == 0:
         app.group.create(Group(group_name="test"))
     app.group.modify_first_group(Group(group_name="Edited group", header="Header edited", footer="Footer edited"))
-
+    old_groups = app.group.get_group_list()
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 
@@ -13,7 +15,9 @@ def test_modify_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(group_name="test"))
     app.group.modify_first_group(Group(group_name="Cool"))
-
+    old_groups = app.group.get_group_list()
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
     
 
 
@@ -21,4 +25,9 @@ def test_modify_group_header(app):
     if app.group.count() == 0:
         app.group.create(Group(group_name="test"))
     app.group.modify_first_group(Group(footer="Yeeep"))
+    old_groups = app.group.get_group_list()
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
+
+
 
