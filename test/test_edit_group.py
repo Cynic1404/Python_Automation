5bcedfbc9ed0638d6715bff53e77e4b879a0bf5d
Nekhaenok <nekhaenok@gmail.com>
2018-03-28 20:06:50 -0700

    modify_some group added

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,26 +1,37 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
+from random import randrange
 
 
 def test_edit_first_group(app):
     if app.group.count() == 0:
         app.group.create(Group(group_name="test"))
     old_groups = app.group.get_group_list()
-    app.group.modify_first_group(Group(group_name="Edited group", header="Header edited", footer="Footer edited"))
-    new_groups = app.group.get_group_list()
+    app.group.modify_first_group(Group(group_name="Edited first group", header="Header edited", footer="Footer edited"))
+    assert len(old_groups) == app.group.count()
+
+
+def test_edit_some_group(app):
+    if app.group.count() == 0:
+        app.group.create(Group(group_name="test"))
+    old_groups = app.group.get_group_list()
+    index = randrange(len(old_groups))
+    group = Group(group_name="Edited some group", header="Header edited", footer="Footer edited")
+    app.group.modify_group_by_index(index, group)
     assert len(old_groups) == app.group.count()
 
 
 def test_modify_group_name(app):
-    group = Group(group_name="Cool")
     if app.group.count() == 0:
         app.group.create(Group(group_name="new group"))
     old_groups = app.group.get_group_list()
-    group.id = old_groups[0].id
-    app.group.modify_first_group(group)
+    index = randrange(len(old_groups))
+    group = Group(group_name="Modified cool name")
+    group.id = old_groups[index].id
+    app.group.modify_group_by_index(index, group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == app.group.count()
-    old_groups[0] = group
+    old_groups[index] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
@@ -28,11 +39,12 @@ def test_modify_group_header(app):
     if app.group.count() == 0:
         app.group.create(Group(group_name="test"))
     old_groups = app.group.get_group_list()
+    index = randrange(len(old_groups))
     name = app.group.get_first_group_name()
-    group = Group(header="Turbo Header", group_name=name)
-    group.id = old_groups[0].id
-    app.group.modify_first_group(group)
+    group = Group(header="Super Header", group_name=name)
+    group.id = old_groups[index].id
+    app.group.modify_group_by_index(index, group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == app.group.count()
-    old_groups[0] = group
+    old_groups[index] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
