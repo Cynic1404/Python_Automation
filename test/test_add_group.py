b7440c72d4305bd2fc99f38006c9c53c6437126d
Nekhaenok <nekhaenok@gmail.com>
2018-04-03 19:54:49 -0700

    @pytest.mark.parametrize

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,9 +1,18 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
+import pytest
+
+testdata = [Group(group_name="My_first_group", header="Header", footer="Footer"),
+            Group(group_name="", header="", footer="")]
+
+@pytest.mark.parametrize("group", testdata, ids = [repr(x) for x in testdata])
 
 
 
-def test_add_group(app):
+def test_add_group(app, group):
+    pass
+
+    """
     old_groups = app.group.get_group_list()
     group = Group(group_name="My_first_group", header="Header", footer="Footer")
     app.group.create(group)
@@ -22,3 +31,4 @@ def test_add_empty_group(app):
     assert len(old_groups) + 1 == app.group.count()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+"""
