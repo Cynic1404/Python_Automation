cd2d5f539da84d2aae14f56260c49258dca8a06f
Nekhaenok <nekhaenok@gmail.com>
2018-04-09 18:52:50 -0700

    Загрузка информации для тестирования из файла data/addgroup

---
+++ data/add_group.py
@@ -0,0 +1,21 @@
+from model.group import Group
+import pytest
+import random
+import string
+
+
+constant = [
+    Group(group_name="Name1", header="Header1", footer="Footer1"),
+    Group(group_name="Name2", header="Header2", footer="Footer2"),
+    Group(group_name="Name3", header="Header3", footer="Footer3")
+]
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters+string.digits+" "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Group(group_name="", header="", footer="")] + [
+    Group(group_name=random_string("name", 10), header = random_string("Header", 20) , footer=random_string("Footer", 20))
+    for i in range(5)
+]
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,19 +1,7 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 import pytest
-import random
-import string
-
-
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters+string.digits+" "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [Group(group_name="", header="", footer="")] + [
-    Group(group_name=random_string("name", 10), header = random_string("Header", 20) , footer=random_string("Footer", 20))
-    for i in range(5)
-]
+from data.add_group import constant as testdata
 
 
 
